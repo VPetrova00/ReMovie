87afdd138b006b7ca53b9dede1ce4f3a8bcc1557
Vesela Petrova <veselapetrova8900@gmail.com>
2023-02-18 02:17:32 +0200

    created recommendation algorithm

--- ReMovie - backend/backend/views.py
+++ ReMovie - backend/backend/views.py
@@ -4,6 +4,10 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
 from .serializers import UserSerializer, MovieSerializer, GenreSerializer, SearchHistorySerializer, \
     FavouriteMoviesSerializer, RatedMoviesSerializer
@@ -50,6 +54,45 @@ class MoviesViewSet(ModelViewSet):
         for movie in movies:
             movies_responses.append(MovieSerializer(movie).data)
         return Response(movies_responses, status=status.HTTP_200_OK)
+
+    def give_recommendations(self, m_id, df_all_movies, X):
+        cos_sim_data = pd.DataFrame(cosine_similarity(X))
+        index_recomm = cos_sim_data.loc[m_id].sort_values(ascending=False).index.tolist()[1:6]
+        movies_recomm = df_all_movies['movie_title'].loc[index_recomm].values
+        result = {'Movies': movies_recomm, 'Index': index_recomm}
+        return result
+
+    @action(detail=False, methods=['get'], url_path='get_related_movies/(?P<movie_id>\d+)')
+    def get_related_movies(self, request, movie_id):
+        movie_index = int(movie_id) - 1
+        all_movies = get_list_or_404(Movie)
+        arr_all_movies = []
+
+        for movie in all_movies:
+            current_movie_id = movie.movie_id
+            current_movie_title = movie.movie_title
+            current_movie_summary = movie.movie_summary
+            current_movie_rating = movie.movie_rating
+            current_movie_release_date = movie.movie_release_date
+            arr_all_movies.append({"movie_id": current_movie_id, "movie_title": current_movie_title,
+                                   "movie_summary": current_movie_summary, 'movie_rating': current_movie_rating,
+                                   'movie_release_date': current_movie_release_date})
+
+        df_all_movies = pd.DataFrame(arr_all_movies,
+                                     columns=['movie_id', 'movie_title', 'movie_summary', 'movie_rating',
+                                              'movie_release_date'])
+        X = np.array(df_all_movies['movie_summary'])
+        model = SentenceTransformer('distilbert-base-nli-mean-tokens')
+        embeddings = model.encode(X, show_progress_bar=True)
+        embed_data = embeddings
+        X = np.array(embed_data)
+
+        recommendations_res = self.give_recommendations(movie_index, df_all_movies, X)
+        movies_to_return = []
+        for r in recommendations_res['Movies']:
+            movie = get_object_or_404(Movie, movie_title=r)
+            movies_to_return.append(MovieSerializer(movie).data)
+        return Response(movies_to_return, status=status.HTTP_200_OK)
 
 
 class SearchHistoryViewSet(ModelViewSet):
